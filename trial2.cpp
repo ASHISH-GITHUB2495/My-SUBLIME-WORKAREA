1b74abd9201c006fd668ef9dce1b1a03ee39e7f5
ASHISH KUMAR YADAV <ashish_business@yahoo.com>
2021-01-17 13:27:40 +0530

    17/01/2021

--- trial2.cpp
+++ trial2.cpp
@@ -1,43 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/* Deleting mid element of stack ... */
-
-
-void solve(stack <int> & s, int k) {   
-	if (k == 1)
-	{	s.pop();
+void josephus(vector<int> v, int index,int k)
+{
+	if(v.size()==1)
+	{
+		cout<<v[0]<<endl;
 		return;
 	}
-   int temp = s.top(); s.pop();
-   solve(s,k-1);
-   s.push(temp);
-   return;
-
-
-}
-
-
-stack<int>  midDel(stack <int> & s) {   
-
-	if (s.size() == 0)
-		return s;
-	int k;
-	(s.size() % 2 != 0) ? k = (s.size() / 2) + 1 : k = s.size() / 2;
-	solve(s, k);
-	return s;
-}
-
-
-
-void printStack(stack <int> s) {       //printing stack with recursion
-	if (s.size() == 0)
-		return;
-	int temp = s.top();
-	s.pop();
-	cout << temp << endl << "---" << endl;
-	printStack(s);
-
+  index = (index + k)%v.size();
+  v.erase(v.begin()+index);
+  josephus(v,index,k);
+ return;
 }
 
 
@@ -52,21 +26,15 @@ int32_t main()
 	ios_base:: sync_with_stdio(false);
 	cin.tie(0);
 //////////////////////////////////////start...............
-	int n, data;
-	cin >> n;
-	stack <int> s;
+	int n,k;
+	cin >> n>>k;
+	int index=0;
+	vector<int> v(n);
+	
+	for(int i=0;i<n;i++)
+		v[i]=i+1;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> data;
-		s.push(data);
-	}
-	int k;
-
-
-	s = midDel(s);
-	printStack(s);
-
+	josephus(v,index,k-1); // k-1 because of 0 index of array
 
 ///////////////////////end-.........................
 
