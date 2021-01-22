c8fefc50696477037dc561c9ce3ca76e4aee84d7
kalyani4785 <surviver4785@gmail.com>
2021-01-22 20:07:46 +0530

    22/01/2021

--- 1.Reversing string using recursion.cpp
+++ 1.Reversing string using recursion.cpp
@@ -1,24 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-void printbro(int n){
-	if(n==0)
-	{
-		cout<<endl;
+void solve(string ip,string &op)
+{
+	if(ip.length()==0)
+	{  
 		return;
 	}
-	cout<<"*"<<" ";
-	printbro(n-1);
+  char temp = ip[0];
+  ip.erase(ip.begin()+0);
+  solve(ip,op);
+  op.push_back(temp);
+  
 }
 
-
-void printPattern(int n){
-	if(n==0)
-		return;
-	printPattern(n-1);
-	printbro(n);
-}
 
 
 int32_t main()
@@ -30,10 +25,13 @@ int32_t main()
 #endif
 	ios_base:: sync_with_stdio(false);
 	cin.tie(0);
-//////////////////////////////////////start............
-int n;
-cin>>n;
-printPattern(n);
+//////////////////////////////////////start...............
+string ip;
+cin>>ip;
+string op = "";
+
+	solve(ip,op); // 
+	cout<<op<<endl;
 
 ///////////////////////end-.........................
 
--- 3.Checking number is pallindrome or not using recursion.cpp
+++ 3.Checking number is pallindrome or not using recursion.cpp
@@ -2,22 +2,13 @@
 using namespace std;
 
 
-void printbro(int n){
-	if(n==0)
-	{
-		cout<<endl;
-		return;
-	}
-	cout<<"*"<<" ";
-	printbro(n-1);
-}
+void reverse(int n,int &g){
+ if( n==0)
+ 	return;
+ int temp = n%10;
+ n=n/10; g=(g*10)+temp;
+ reverse(n,g);
 
-
-void printPattern(int n){
-	if(n==0)
-		return;
-	printPattern(n-1);
-	printbro(n);
 }
 
 
@@ -30,10 +21,21 @@ int32_t main()
 #endif
 	ios_base:: sync_with_stdio(false);
 	cin.tie(0);
-//////////////////////////////////////start............
+//////////////////////////////////////start...............
 int n;
 cin>>n;
-printPattern(n);
+
+int g=0;
+reverse(n,g);
+
+if(n==g)
+	cout<<"yes"<<endl;
+else
+	cout<<"no"<<endl;
+
+
+
+ 
 
 ///////////////////////end-.........................
 
